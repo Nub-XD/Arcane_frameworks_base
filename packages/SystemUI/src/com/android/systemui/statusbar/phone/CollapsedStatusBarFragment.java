e62cb50f0e3eb78b43db131e1387fc5ee7e4a204
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-09-14 21:21:33 +0000

    UI: rework interpolators

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -41,6 +41,7 @@ import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.NetworkController;
 import com.android.systemui.statusbar.policy.NetworkController.SignalCallback;
 import com.android.systemui.tuner.TunerService;
+import com.android.systemui.volume.SystemUIInterpolators;
 
 /**
  * Contains the collapsed status bar and handles hiding/showing based on disable flags
@@ -335,7 +336,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
                 .alpha(0f)
                 .setDuration(160)
                 .setStartDelay(0)
-                .setInterpolator(Interpolators.ALPHA_OUT)
+                .setInterpolator(new SystemUIInterpolators.LogAccelerateInterpolator(200, 0))
                 .withEndAction(() -> v.setVisibility(state));
     }
 
@@ -359,7 +360,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         v.animate()
                 .alpha(1f)
                 .setDuration(FADE_IN_DURATION)
-                .setInterpolator(Interpolators.ALPHA_IN)
+                .setInterpolator(new SystemUIInterpolators.LogDecelerateInterpolator(800f, 2.1f, 0))
                 .setStartDelay(FADE_IN_DELAY)
 
                 // We need to clean up any pending end action from animateHide if we call
@@ -371,7 +372,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         if (mKeyguardStateController.isKeyguardFadingAway()) {
             v.animate()
                     .setDuration(mKeyguardStateController.getKeyguardFadingAwayDuration())
-                    .setInterpolator(Interpolators.LINEAR_OUT_SLOW_IN)
+                    .setInterpolator(new SystemUIInterpolators.LogAccelerateInterpolator(200, 0))
                     .setStartDelay(mKeyguardStateController.getKeyguardFadingAwayDelay())
                     .start();
         }
