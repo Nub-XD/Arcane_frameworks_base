4785442bd414d7e35664d9f524447db76f6ed6a1
mark <khevinrogacion@gmail.com>
2021-08-23 15:01:57 +0800

    fwb:Allow to suppress notifications sound/vibration if screen is active[1/2]
    Co-authored-by: Ezio Lacandia Bijelkic <brabus84@gmail.com>
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5200,6 +5200,14 @@ public final class Settings {
         public static final String DISPLAY_COLOR_ADJUSTMENT = "display_color_adjustment";
 
         /**
+         * Whether to play notification sound and vibration if screen is ON
+         * 0 - never
+         * 1 - always
+         * @hide
+         */
+        public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
+        
+        /**
          * The current custom picture adjustment values as a delimited string
          */
         public static final String DISPLAY_PICTURE_ADJUSTMENT =
