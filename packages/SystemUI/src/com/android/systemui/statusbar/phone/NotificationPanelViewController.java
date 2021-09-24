8c1d162ccd189948b00e5f0f8a971eba6f3a4a4d
dlwlrma123 <alexfinhart@gmail.com>
2021-09-24 13:35:56 +0800

    SystemUI: Introduce OOS style notification clear all button
    @neobuddy89: Updated colors, removed unused dimens.
    
    Co-Authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -2172,6 +2172,7 @@ public class NotificationPanelViewController extends PanelViewController {
             alpha *= mClockPositionResult.clockAlpha;
         }
         mNotificationStackScroller.setAlpha(alpha);
+        mStatusBar.updateDismissAllVisibility(true);
     }
 
     private float getFadeoutAlpha() {
